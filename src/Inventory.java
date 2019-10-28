d16341d3f3b96e24a324f4b21aa579b77bc76918
Isse Nur <issenur@gmail.com>
2019-10-28 09:15:55 -0500

    added more code to product class and inventory class

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,30 +1,92 @@
+import java.io.Serializable;
 import java.util.LinkedList;
-
-public class Inventory {
-	private LinkedList<Product> productList = new LinkedList<Product>();
-	private static Inventory inventory;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * 
+ * 
+ * The following code is based on code from Brahma Dathan 
+ * lecture notes
+ *
+ * The collection class for Product Objects
+ * 
+ *
+ */
+public class Inventory{
+	private LinkedList productList = new LinkedList();
+	private static Inventory inventory;	
 	
-	public static Inventory getInstance() {
-		//TODO
-	}
-	
+	/*
+     * Private constructor for singleton pattern
+     * 
+     */
 	private Inventory() {
-		//TODO
 	}
 	
-	private boolean addProduct(Product product) {
-		//TODO
+	/**
+     * Supports the singleton pattern
+     * 
+     * @return the singleton object
+     */
+	public static Inventory getInstance() {
+		if(inventory == null) {
+			return (inventory = new Inventory());
+		}
 	}
 	
+	/**
+     * Checks whether a product with a given product id exists.
+     * 
+     * @param productId
+     *            the id of the product
+     * @return true iff the product exists
+     * 
+     */
+	private Product findProduct(String productId){
+		for (Iterator iterator = productList.iterator(); iterator.hasNext();) {
+			Product product = (Product) iterator.next();
+			if (product.getId().equals(productId)) {
+				return product;
+			}
+		}
+		return null;
+	}
+	
+	/**
+     * Removes a product from the inventory
+     * 
+     * @param productId
+     *            product id
+     * @return true iff product could be removed
+     */
 	private boolean removeProduct(String productId) {
-		//TODO
+		Product product = findProduct(productId);
+		if (product == null) {
+			return false;
+		} else {
+			return productList.remove(product);
+		}
 	}
 	
-	private Product findProduct(String productId) {
-		//TODO
+	 /**
+     * Add a product into the collection
+     * 
+     * @param product
+     *            the product to be added
+     * @return true iff the product could be added. Currently always true
+     */
+	private boolean addProduct(Product product) {
+		productList.add(product);
+		return true;
 	}
 	
+	/**
+     * Returns an iterator to all product
+     * 
+     * @return iterator to the collection
+     */
 	private Iterator getProducts() {
-		//TODO
+		return productList.iterator();
 	}
 }
